62382393c8abf7ccab6f129663786f1d0c1776e3
vivihe <woshihehaha@163.com>
2015-09-02 21:30:41 +0800

    Define cor to replace corr.

--- R/cor.R
+++ R/cor.R
@@ -1,42 +1,48 @@
 
 #' Correlation Computing for \code{zoo} or \code{zoocat} Objects
 #' 
-#' Compute correlations for \code{zoo} or \code{zoocat} objects.
+#' Compute corelations for \code{zoo} or \code{zoocat} objects.
 #' 
 #' @examples
 #' 
 #' x <- zoo(c(1, 3, 2, 4, 5))
 #' y <- zoo(c(12, 30, 2, 46, 5))
-#' corr(x, y)
+#' cor(x, y)
 #' 
 #' x <- matrix(1 : 20, nrow = 5)
 #' md <- mlydata(x, year = 1991 : 1995, month = c(2, 3, 5, 6))
-#' corr(md, md)
-#' corr(md)
-#' corr(md, method = 'kendall')
+#' cor(md, md)
+#' cor(md)
+#' cor(md, method = 'kendall')
 #' 
 #' x <- matrix(1 : 20, nrow = 5)
 #' colAttr <- data.frame(month = c(2, 3, 5, 6), name = 'x')
 #' zc <- zoocat(x, order.by = 1991 : 1995, colattr = colAttr)
 #' y <- zoo(c(3, 5, 4, 6, 3), order.by = 1991 : 1995)
-#' corr(zc, y, method = 'kendall')
-#' corr(zc, md)
-#' corr(zc, method = 'kendall')
+#' cor(zc, y, method = 'kendall')
+#' cor(zc, md)
+#' cor(zc, method = 'kendall')
 #' 
 #' 
 #' @export
-#' @rdname corr
-#' @name corr
+#' @rdname cor
+#' @name cor
 #' @param x A \code{zoo} or \code{zoocat} object.
 #' @param y NULL or a \code{zoo} or \code{zoocat} object. If NULL, x will be used.
 #' @param ... Other arguments for function \code{cor}.
-corr <- function (x, y = NULL, ...) {
-    UseMethod('corr')
+cor <- function (x, y = NULL, ...) {
+    UseMethod('cor')
 }
 
-#' @rdname corr
+
 #' @export
-corr.zoo <- function (x, y = NULL, ...) {
+cor.default <- function (x, y = NULL, ...) {
+    stats::cor(x, y, ...)
+}
+
+#' @rdname cor
+#' @export
+cor.zoo <- function (x, y = NULL, ...) {
     stopifnot(is.null(y) | is.zoo(y))
     if (!is.null(y)) {
         stopifnot(length(index(x)) == length(index(y)))
@@ -48,14 +54,14 @@ corr.zoo <- function (x, y = NULL, ...) {
         }
     }
     x <- as.matrix(x)
-    ret <- cor(x, y, ...)
+    ret <- stats::cor(x, y, ...)
     return(ret)
 }
 
 
-#' @rdname corr
+#' @rdname cor
 #' @export
-corr.zoocat <- function (x, y = NULL, ...) {
+cor.zoocat <- function (x, y = NULL, ...) {
     stopifnot(is.null(y) | is.zoo(y))
     if (!is.null(y)) {
         stopifnot(length(index(x)) == length(index(y)))
@@ -69,10 +75,10 @@ corr.zoocat <- function (x, y = NULL, ...) {
     cAttr <- cattr(x)
     x <- as.matrix(x)
     if (!is.null(y) & (is.null(dim(y)) || ncol(y) == 1)) {
-        ccvec <- cor(x, y, ...)
+        ccvec <- stats::cor(x, y, ...)
         ret <- data.frame(cAttr, cor.value = ccvec)
     } else {
-        ret <- cor(x, y, ...)
+        ret <- stats::cor(x, y, ...)
     }
     return(ret)
 }
