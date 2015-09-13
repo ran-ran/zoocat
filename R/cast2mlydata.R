6028b4141c9f650ebc454f48419739e37be0590c
vivihe <woshihehaha@163.com>
2015-09-13 13:51:35 +0800

    modify help page.

--- R/cast2mlydata.R
+++ R/cast2mlydata.R
@@ -1,20 +1,27 @@
 
-#' Cast a Data Frame as a \code{mlydata} Object
+#' Cast a Data Frame as a \code{mlydata} or \code{mlydataList} Object
 #' 
-#' Cast a data frame as a \code{mlydata} object.
+#' Cast a data frame as a \code{mlydata} or \code{mlydataList} object.
 #' 
-#' @param x A data frame.
-#' @param year.var The name of the column to be treated as the index of the mlydata object.
-#' @param month.var The name of the column which stores month.
-#' @param value.var The name of the column which stores the values.
-#' @param variable.var The name of the column which stores the variable names.
+#' If the length of \code{value.var} is larger than 1, each variable in \code{value.var}
+#' will be casted as a \code{mlydata} object, and a \code{mlydataList} will be returned.\cr
+#' If the \code{variable.var} is specified, each value in \code{variable.var} corresponding 
+#' to a variable, and corresponding row will be casted into a \code{mlydata} object in 
+#' the returned \code{mlydataList} object.
+#' 
+#' @param x a data frame.
+#' @param year.var the name of the column which stores year.
+#' @param month.var the name of the column which stores month.
+#' @param value.var the name of the column which stores values. Can have 
+#' several elements
+#' @param variable.var the name of the column which stores the variable names.
 #' @param fun.aggr Aggregation function needed if variables do not identify a single observation
 #' for each output cell. Defaults to length (with a message) if needed but not specified.
 #' @return A \code{mlydata} object.
 #' @examples
 #' 
 #' x <- data.frame(year = rep(1991 : 1993, each = 3), month = rep(3 : 1, 3), value = 1 : 9)
-#' md <- cast2mlydata(x, index.var = 'year', month.var = 'month', value.var = 'value', fun.aggr = mean)
+#' md <- cast2mlydata(x, year.var = 'year', month.var = 'month', value.var = 'value', fun.aggr = mean)
 #' 
 #' data(sst)
 #' cast2mlydata(sst, value.var = c('nino12', 'nino3'))
