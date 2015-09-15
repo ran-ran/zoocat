e27a158b094e90f61f1cba9ed6c6a7e1eda2a223
vivihe <woshihehaha@163.com>
2015-09-15 13:21:43 +0800

    Generate methods of mlydataList.

--- R/methods_mlydataList.R
+++ R/methods_mlydataList.R
@@ -1,7 +1,7 @@
 
+#' @export 
 #' @rdname window
-#' @export
-window.mlydataList <- function (x, ...) {
+'window.mlydataList' <- function (x, ...) {
     for (i in 1 : length(x)) {
         x[[i]] <- window(x[[i]], ...)
     }
@@ -9,26 +9,19 @@ window.mlydataList <- function (x, ...) {
 }
 
 
+#' @export 
 #' @rdname expand
-#' @export
-expand.mlydataList <- function (x, ...) {
+'cummax.mlydataList' <- function (x, ...) {
     for (i in 1 : length(x)) {
-        x[[i]] <- expand(x[[i]], ...)
+        x[[i]] <- cummax(x[[i]], ...)
     }
     return(x)
 }
 
-#' @export
-na.trim.mlydataList <- function (object, ...) {
-    for (i in 1 : length(object)) {
-        object[[i]] <- na.trim(object[[i]], ...)
-    }
-    return(object)
-}
 
-
-#' @export
-normalize.mlydataList <- function (x, ...) {
+#' @export 
+#' @rdname normalize
+'normalize.mlydataList' <- function (x, ...) {
     for (i in 1 : length(x)) {
         x[[i]] <- normalize(x[[i]], ...)
     }
@@ -36,5 +29,173 @@ normalize.mlydataList <- function (x, ...) {
 }
 
 
+#' @export 
+'cummin.mlydataList' <- function (x, ...) {
+    for (i in 1 : length(x)) {
+        x[[i]] <- cummin(x[[i]], ...)
+    }
+    return(x)
+}
 
+
+#' @export 
+'cumprod.mlydataList' <- function (x, ...) {
+    for (i in 1 : length(x)) {
+        x[[i]] <- cumprod(x[[i]], ...)
+    }
+    return(x)
+}
+
+
+#' @export 
+'cumsum.mlydataList' <- function (x, ...) {
+    for (i in 1 : length(x)) {
+        x[[i]] <- cumsum(x[[i]], ...)
+    }
+    return(x)
+}
+
+
+#' @export 
+'diff.mlydataList' <- function (x, ...) {
+    for (i in 1 : length(x)) {
+        x[[i]] <- diff(x[[i]], ...)
+    }
+    return(x)
+}
+
+
+#' @export 
+'head.mlydataList' <- function (x, ...) {
+    for (i in 1 : length(x)) {
+        x[[i]] <- head(x[[i]], ...)
+    }
+    return(x)
+}
+
+
+#' @export 
+'lag.mlydataList' <- function (x, ...) {
+    for (i in 1 : length(x)) {
+        x[[i]] <- lag(x[[i]], ...)
+    }
+    return(x)
+}
+
+
+#' @export 
+'na.aggregate.mlydataList' <- function (object, ...) {
+    for (i in 1 : length(object)) {
+        object[[i]] <- na.aggregate(object[[i]], ...)
+    }
+    return(object)
+}
+
+
+#' @export 
+'na.approx.mlydataList' <- function (object, ...) {
+    for (i in 1 : length(object)) {
+        object[[i]] <- na.approx(object[[i]], ...)
+    }
+    return(object)
+}
+
+
+#' @export 
+'na.contiguous.mlydataList' <- function (object, ...) {
+    for (i in 1 : length(object)) {
+        object[[i]] <- na.contiguous(object[[i]], ...)
+    }
+    return(object)
+}
+
+
+#' @export 
+'na.fill.mlydataList' <- function (object, ...) {
+    for (i in 1 : length(object)) {
+        object[[i]] <- na.fill(object[[i]], ...)
+    }
+    return(object)
+}
+
+
+#' @export 
+'na.spline.mlydataList' <- function (object, ...) {
+    for (i in 1 : length(object)) {
+        object[[i]] <- na.spline(object[[i]], ...)
+    }
+    return(object)
+}
+
+
+#' @export 
+'na.trim.mlydataList' <- function (object, ...) {
+    for (i in 1 : length(object)) {
+        object[[i]] <- na.trim(object[[i]], ...)
+    }
+    return(object)
+}
+
+
+#' @export 
+'rollapply.mlydataList' <- function (data, ...) {
+    for (i in 1 : length(data)) {
+        data[[i]] <- rollapply(data[[i]], ...)
+    }
+    return(data)
+}
+
+
+#' @export 
+'rollmax.mlydataList' <- function (x, ...) {
+    for (i in 1 : length(x)) {
+        x[[i]] <- rollmax(x[[i]], ...)
+    }
+    return(x)
+}
+
+
+#' @export 
+'rollmean.mlydataList' <- function (x, ...) {
+    for (i in 1 : length(x)) {
+        x[[i]] <- rollmean(x[[i]], ...)
+    }
+    return(x)
+}
+
+
+#' @export 
+'rollmedian.mlydataList' <- function (x, ...) {
+    for (i in 1 : length(x)) {
+        x[[i]] <- rollmedian(x[[i]], ...)
+    }
+    return(x)
+}
+
+
+#' @export 
+'rollsum.mlydataList' <- function (x, ...) {
+    for (i in 1 : length(x)) {
+        x[[i]] <- rollsum(x[[i]], ...)
+    }
+    return(x)
+}
+
+
+#' @export 
+'scale.mlydataList' <- function (x, ...) {
+    for (i in 1 : length(x)) {
+        x[[i]] <- scale(x[[i]], ...)
+    }
+    return(x)
+}
+
+
+#' @export 
+'tail.mlydataList' <- function (x, ...) {
+    for (i in 1 : length(x)) {
+        x[[i]] <- tail(x[[i]], ...)
+    }
+    return(x)
+}
 
---
+++ others/generate_mlydataList_methods.R
@@ -0,0 +1,70 @@
+
+fout <- 'E:/Codes/zoocat/R/methods_mlydataList.R'
+
+if (file.exists(fout)) {
+    file.remove(fout)
+}
+
+fcon <- file(fout, 'w')
+
+methodNames <- matrix(
+                 c(
+                 'window', 'x', 'window',
+                 'expand', 'x', 'expand', 
+                 'normalize', 'x', 'normalize',
+                 'cummin', 'x', '',
+                 'cumprod', 'x', '',
+                 'cumsum', 'x', '',
+                 'diff', 'x', '', 
+                 'head', 'x', '',
+                 'lag', 'x', '',
+                 'na.aggregate', 'object', '',
+                 'na.approx', 'object', '',
+                 'na.contiguous', 'object', '',
+                 'na.fill', 'object', '',
+                 'na.spline', 'object', '',
+                 'na.trim', 'object', '',
+                 'rollapply', 'data', '',
+                 'rollmax', 'x', '',
+                 'rollmean', 'x', '',
+                 'rollmedian', 'x', '',
+                 'rollsum', 'x', '',
+                 'scale', 'x', '',
+                 'tail', 'x', ''
+                 ),
+                 ncol = 3, byrow = TRUE)
+
+
+for (i in 1 : nrow(methodNames)) {
+    mtd <- methodNames[i, 1]
+    obj <- methodNames[i, 2]
+    rdname <- methodNames[i, 3]
+    
+    cat("
+#' @export 
+", 
+        sep = '',
+        file = fcon)
+
+if (nchar(rdname) != 0) {
+    cat("#' @rdname ", rdname, "\n",
+        sep = '',
+        file = fcon)
+}
+
+    cat(
+"'", mtd, ".mlydataList' <- function (", obj, ", ...) {
+    for (i in 1 : length(", obj, ")) {
+        ", obj, "[[i]] <- ", mtd, "(", obj, "[[i]], ...)
+    }
+    return(", obj, ")
+}
+
+",
+
+        sep = '',
+        file = fcon)
+}
+
+
+close(fcon)
