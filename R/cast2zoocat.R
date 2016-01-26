ef467b6d8c169fe337c1e0d4ddb491d4aa3df9f4
ran-ran <heran198311@163.com>
2016-01-26 16:21:21 +0800

    add cast2zoomly

--- R/cast2zoocat.R
+++ R/cast2zoocat.R
@@ -108,3 +108,18 @@ cast2zoocat <- function (x, index.var, value.var, attr.var = NULL, fun.aggregate
 
 
 
+#' Cast a data frame as a zoomly object
+#' @export
+#' @param x a data frame
+#' @param ... other arguments for cast2zoocat
+#' @examples
+#' df <- data.frame(year = rep(1991 : 1995, each = 24), month = rep(1 : 12, 10),
+#'                  varname = rep(c('a', 'b'), each = 12), city = rep(1 : 3, each = 40),
+#'                  value = 1 : 120)
+#' cast2zoomly(df, index.var = 'year', value.var = 'value')
+#' 
+cast2zoomly <- function (x, ...) {
+    zc <- cast2zoocat(x, ...)
+    return(as.zoomly(zc))
+}
+
