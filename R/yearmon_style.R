9c627328219f6c2863a0785404a0c26cf73a92d3
ran-ran <heran198311@163.com>
2016-01-26 20:01:23 +0800

    add melt_month and cast_month

---
+++ R/yearmon_style.R
@@ -0,0 +1,51 @@
+#' Translate zoomly objects to yearmon format
+#' @param x a zoomly object.
+#' @examples 
+#' x <- matrix(1 : 20, nrow = 5)
+#' zm <- zoomly(x, order.by = 1991 : 1995, 
+#'              colattr = data.frame(month = c(2, 3, 5, 6)))
+#' melt_month(zm)
+#' 
+#' x <- matrix(1 : 20, nrow = 5)
+#' colAttr <- data.frame(month = c(2, 3, 5, 6), name = c(rep('xxx', 3), 'yyy'))
+#' zc <- zoocat(x, order.by = 1991 : 1995, colattr = colAttr)
+#' melt_month(as.zoomly(zc))
+#' 
+melt_month <- function (x) {
+    df <- melt(x, value.name = 'value')
+    ym <- df$year + (df$month - 1) / 12 
+    ym <- as.yearmon(ym)
+    
+    col.yr.mon <- which(colnames(df) %in% c('year', 'month'))
+    df <- df[, -1 * col.yr.mon]
+    df.ym <- data.frame(yrmon = ym, df)
+    
+    if (ncol(df.ym) == 2) {
+        ret <- zoo(df.ym[, 2], order.by = df.ym$ym)
+    } else {
+        ret <- cast2zoocat(df.ym, index.var = 'yrmon', 
+                           value.var = 'value')
+    }
+    return(ret)
+}
+
+
+#' Cast month of a zoocat object
+#' 
+#' @examples
+#' x <- matrix(1 : 20, nrow = 5)
+#' colAttr <- data.frame(month = c(2, 3, 5, 6), name = c(rep('xxx', 3), 'yyy'))
+#' zc <- zoocat(x, order.by = 1991 : 1995, colattr = colAttr)
+#' zmelt <- melt_month(as.zoomly(zc))
+#' cast_month(zmelt)
+cast_month <- function (x) {
+    df <- melt(x, value.name = 'value', index.name = 'index')
+    ind <- as.Date(df$index)
+    df$year <- format(ind, "%Y")
+    df$month <- format(ind, "%m")
+    df$index <- NULL
+    ret <- cast2zoocat(df, index.var = 'year', value.var = 'value')
+    return(ret)
+}
+
+
