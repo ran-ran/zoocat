d637b410d93f3d241b947b03fd41cac9d118affb
Ran-Ran He <heranran2006@163.com>
2016-09-30 09:20:20 +0800

    add test codes for ops.zoocat

--- tests/testthat/test_ops.R
+++ tests/testthat/test_ops.R
@@ -14,6 +14,13 @@ test_that('Test Ops.zoocat', {
     cattr(zc2)[1, 1] <- 5
     expect_error(zc5 <- zc + zc2, info = 'cattr of objects must be identical.')
     zobj <- as.zoo(zc2)
-    zc + zobj
+    
+    x <- matrix(1 : 20, nrow = 5)
+    colAttr <- data.frame(month = c(2, 3, 5, 6), name = c(rep('xxx', 3), 'yyy'))
+    zc <- zoocat(x, order.by = 1991 : 1995, colattr = colAttr)
+    zc2 <- zc[2 : 3, ]
+    s1 <- zc + zc2
+    s2 <- zc + zc
+    expect_identical(s1, s2[2 : 3, ])
 })
 
